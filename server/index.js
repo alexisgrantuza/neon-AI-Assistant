ae9046fd41bc5a13b67d718b3b42ed3fae00df79
Alexis Grantuza <alexisgrantuza@gmail.com>
2024-12-19 23:34:52 +0800

    final-version

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://strong-marshmallow-3d72fc.netlify.app/", // Replace with your frontend's URL
+    origin: "http://localhost:5173", // Replace with your frontend's URL
     credentials: true,
   })
 );
